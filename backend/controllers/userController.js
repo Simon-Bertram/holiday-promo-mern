d7b2bd8c1096499bb6f6c759d4d6cf50aca92d5a
Simon-Bertram <simonbertram@hotmail.com>
2025-01-14 23:24:14 +0000

    Connect user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,4 +9,46 @@ const authUser = asyncHandler((req, res) => {
   });
 });
 
-export { authUser };
+// @desc: Register a new user
+// route: POST /api/users
+// access: Public
+const registerUser = asyncHandler((req, res) => {
+  res.status(200).json({
+    message: "Register User",
+  });
+});
+
+// @desc: Logout user
+// route: POST /api/users/logout
+// access: Public
+const logoutUser = asyncHandler((req, res) => {
+  res.status(200).json({
+    message: "Logout User",
+  });
+});
+
+// @desc: get user profile
+// route: GET /api/users/profile
+// access: Private
+const getUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({
+    message: "User profile",
+  });
+});
+
+// @desc: Update user profile
+// route: PUT /api/users/profile
+// access: Private
+const updateUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({
+    message: "Update User profile",
+  });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
