59a728b827cb422c5259d300bd58185c565f600a
Simon-Bertram <simonbertram@hotmail.com>
2025-01-19 20:36:01 +0000

    Create Hero component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,12 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <div>
-      <h1>The Best Holiday Promotions</h1>
+    <div className="py-5">
+      <div className="container">
+        <h1>The Best Holiday Promotions</h1>
+        <Hero />
+      </div>
     </div>
   );
 }
