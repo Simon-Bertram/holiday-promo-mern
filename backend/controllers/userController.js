6a4964ae3e32deda44919c9b5717a40e7e108df0
Simon-Bertram <simonbertram@hotmail.com>
2025-01-19 17:33:43 +0000

    Complete logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,8 +52,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route: POST /api/users/logout
 // access: Public
 const logoutUser = asyncHandler((req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({
-    message: "Logout User",
+    message: "User logged out",
   });
 });
 
