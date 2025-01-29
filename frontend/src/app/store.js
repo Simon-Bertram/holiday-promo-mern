87d6af6c2e8b3419ca2a32e6c9062f99d5eb0775
Simon-Bertram <simonbertram@hotmail.com>
2025-01-29 14:55:25 +0000

    Create apiSlice and bring it into store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
+import { apiSlice } from "../slices/apiSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       auth: authReducer,
+      [apiSlice.reducerPath]: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
   });
 };
