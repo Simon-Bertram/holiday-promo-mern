843f62c88d6cfedd1292a4bf88ab8c60be37696d
Simon-Bertram <simonbertram@hotmail.com>
2025-02-20 09:41:13 +0000

    Complete subscriber controller CRUD methods

--- backend/controllers/subscriberController.js
+++ backend/controllers/subscriberController.js
@@ -6,24 +6,62 @@ import { protect } from "../middleware/authMiddleware.js";
 // route: GET /api/subscribers
 // access: Private - accessible to moderators and admins only
 const getSubscribers = asyncHandler(async (req, res) => {
-  const subscribers = await User.find({ role: "user" });
+  const subscribers = await User.find({ role: "user" }).select(
+    "name email isVerified"
+  );
+  if (!subscribers) {
+    res.status(404);
+    throw new Error("Problem fetching subscribers");
+  }
   res.status(200).json(subscribers);
 });
 
 // @desc    Get a single subscriber
 // @route   GET /api/subscribers/:id
 // @access  Private - accessible to moderators and admins only
+const getSubscriber = asyncHandler(async (req, res) => {
+  const subscriber = await User.findById(req.params.id, {
+    role: "user",
+  }).select("name email isVerified");
+  if (!subscriber) {
+    res.status(404);
+    throw new Error("Problem fetching subscriber");
+  }
+  res.status(200).json(subscriber);
+});
 
 // @desc    Create a new subscriber
 // @route   POST /api/subscribers
 // @access  Private (protected via middleware)
+const createSubscriber = asyncHandler(async (req, res) => {
+  const { name, email } = req.body;
+  const subscriber = await User.create({ name, email, role: "user" });
+  res.status(201).json(subscriber);
+});
 
 // @desc    Update a subscriber
 // @route   PUT /api/subscribers/:id
 // @access  Private (protected via middleware)
-
-// @desc    Delete a subscriber
+const updateSubscriber = asyncHandler(async (req, res) => {
+  const { name, email } = req.body;
+  const subscriber = await User.findByIdAndUpdate(
+    req.params.id,
+    { name, email },
+    { new: true }
+  );
+  res.status(200).json(subscriber);
+});
 // @route   DELETE /api/subscribers/:id
 // @access  Private (protected via middleware)
+const deleteSubscriber = asyncHandler(async (req, res) => {
+  const subscriber = await User.findByIdAndDelete(req.params.id);
+  res.status(200).json(subscriber);
+});
 
-export { getSubscribers };
+export {
+  getSubscribers,
+  getSubscriber,
+  createSubscriber,
+  updateSubscriber,
+  deleteSubscriber,
+};
