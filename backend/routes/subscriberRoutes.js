22a140f82c7c1e23b757b30299e2b953990b336d
Simon-Bertram <simonbertram@hotmail.com>
2025-02-20 09:44:30 +0000

    Create routes for subscriber CRUD and protect with the authorize roles middleware

--- backend/routes/subscriberRoutes.js
+++ backend/routes/subscriberRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { getSubscribers } from "../controllers/subscriberController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import {
+  getSubscribers,
+  getSubscriber,
+  createSubscriber,
+  updateSubscriber,
+  deleteSubscriber,
+} from "../controllers/subscriberController.js";
+import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", protect, getSubscribers);
+// Apply both middlewares to all routes in this router.
+// Only users with role 'moderator' or 'admin' will be allowed.
+router.use(protect, authorizeRoles("moderator", "admin"));
+
+router.route("/").get(getSubscribers).post(createSubscriber);
+
+router
+  .route("/:id")
+  .get(getSubscriber)
+  .put(updateSubscriber)
+  .delete(deleteSubscriber);
 
 export default router;
