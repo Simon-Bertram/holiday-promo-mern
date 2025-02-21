7a1d387c835a11996ab2bc6a9540a911d5c9a79c
Simon-Bertram <simonbertram@hotmail.com>
2025-02-21 12:42:55 +0000

    Simplify the authSlice removing localStorage options as we're using cookies and load state will be used transferred to RTK query

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -10,23 +10,12 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
-      if (typeof window !== "undefined") {
-        localStorage.setItem("userInfo", JSON.stringify(action.payload));
-        localStorage.setItem("token", action.payload.token);
-      }
     },
     logout: (state) => {
       state.userInfo = null;
-      if (typeof window !== "undefined") {
-        localStorage.removeItem("userInfo");
-        localStorage.removeItem("token");
-      }
-    },
-    setLoading: (state, action) => {
-      state.isLoading = action.payload;
     },
   },
 });
 
-export const { setCredentials, logout, setLoading } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;
